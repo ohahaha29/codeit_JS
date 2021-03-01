5a0db6cf7cbb1036f0a9a2106c88b64606bef0c9
ohahaha29 <ohahaha29@gmail.com>
2021-03-01 15:35:44 +0900

    함수 선언

--- in_javascript/index.js
+++ in_javascript/index.js
@@ -1,11 +1,26 @@
-//변수 선언
-let espresso = 10;
-let milk = 170;
-let chocolateSyrup = 50;
-let whippedCream = 60;
+//함수 선언
+function printChorus() {
+    console.log('무궁화 삼천리 화려 강산');
+    console.log('대한 사람 대한으로 길이 보전하세');
+};
 
-// 메뉴별 칼로리 테스트
-console.log(espresso); // 에스프레소 칼로리
-console.log(espresso + milk); // 라떼 칼로리
-console.log(espresso + chocolateSyrup + milk); // 모카 칼로리
-console.log(espresso + chocolateSyrup + milk + whippedCream); // 모카(휘핑 추가) 칼로리
+//함수 호출
+console.log('1절');
+console.log('동해 물과 백두산이 마르고 닳도록');
+console.log('하느님이 보우하사 우리나라 만세');
+printChorus();
+
+console.log('2절');
+console.log('남산 위에 저 소나무 철갑을 두른 듯');
+console.log('바람서리 불변함은 우리 기상일세');
+printChorus();
+
+console.log('3절');
+console.log('가을 하늘 공활한데 높고 구름 없이');
+console.log('밝은 달은 우리 가슴 일편단심일세');
+printChorus();
+
+console.log('4절');
+console.log('이 기상과 이 맘으로 충성을 다하여');
+console.log('괴로우나 즐거우나 나라 사랑하세');
+printChorus();
