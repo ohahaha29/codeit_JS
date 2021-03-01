65cef670fbd14b02012b483675e65a33fbd99f4f
ohahaha29 <ohahaha29@gmail.com>
2021-03-01 15:50:07 +0900

    파라미터

--- in_javascript/index.js
+++ in_javascript/index.js
@@ -1,26 +1,11 @@
-//함수 선언
-function printChorus() {
-    console.log('무궁화 삼천리 화려 강산');
-    console.log('대한 사람 대한으로 길이 보전하세');
+//파라미터
+function welcome(name) {
+    console.log("안녕하세요 " + name + "님!");
 };
 
-//함수 호출
-console.log('1절');
-console.log('동해 물과 백두산이 마르고 닳도록');
-console.log('하느님이 보우하사 우리나라 만세');
-printChorus();
+welcome("코드잇");
+welcome("메리메리");
+welcome("코딩");
 
-console.log('2절');
-console.log('남산 위에 저 소나무 철갑을 두른 듯');
-console.log('바람서리 불변함은 우리 기상일세');
-printChorus();
-
-console.log('3절');
-console.log('가을 하늘 공활한데 높고 구름 없이');
-console.log('밝은 달은 우리 가슴 일편단심일세');
-printChorus();
-
-console.log('4절');
-console.log('이 기상과 이 맘으로 충성을 다하여');
-console.log('괴로우나 즐거우나 나라 사랑하세');
-printChorus();
+/*함수 선언 부분에서 함수 이름 바로 뒤에 오는 소괄호 내부에 파라미터를 작성하고
+함수 내부에서 파라미터를 변수처럼 사용하게 되면 함수를 호출할 때 소괄호 안에 입력한 값이 파라미터에 전달되는 개념*/
