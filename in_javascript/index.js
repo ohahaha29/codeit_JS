cd89c171e6e7601859c32f26be25c8d1843dca8b
ohahaha29 <ohahaha29@gmail.com>
2021-03-01 15:09:20 +0900

    변수선언

--- in_javascript/index.js
+++ in_javascript/index.js
@@ -1,1 +1,8 @@
-console.log("hello codeit")
+//변수 선언
+let espressoPrice = 3000;
+let lattePrice = 4300;
+let mochaPrice = 3000;
+
+console.log(espressoPrice * 2);
+console.log(lattePrice * 4 + mochaPrice * 2);
+console.log(lattePrice + mochaPrice);
