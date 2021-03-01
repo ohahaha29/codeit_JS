91b8db62eb2c5dd240e49f2df4061b2a0bb3aaef
ohahaha29 <ohahaha29@gmail.com>
2021-03-01 16:12:25 +0900

    여러 개의 파라미터

--- in_javascript/index.js
+++ in_javascript/index.js
@@ -1,15 +1,18 @@
-//파라미터
-function teraToGiga(volume) {
-    console.log(volume + "TB는");
-    console.log(volume * 1024 + "GB 입니다.");
+//여러 개의 파라미터
+function printSum(num1, num2) {
+    console.log(num1 + num2);
 };
 
-function teraToMega(volume) {
-    console.log(volume + "TB는");
-    console.log(volume * 1024 * 1024 + "MB 입니다.");
+printSum(10, 5);
+printSum(123, 4);
+
+function introduce(name, birth, nationality, job) {
+    console.log("안녕하세요! 반갑습니다.");
+    console.log("제 이름은 " + name + "입니다.");
+    console.log("생년월일은 " + birth + "이고,");
+    console.log("국적은 " + nationality + "입니다.");
+    console.log("직업은 " + job + "입니다.");
+    console.log("잘 부탁드립니다.");
 };
 
-// TB -> GB 테스트
-teraToGiga(2);
-// TB -> MB 테스트
-teraToMega(2);
+introduce("코드잇", "17.05.18", "한국", "프로그래머");
