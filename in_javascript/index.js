acd0a49d98b6dfaa630f47ae2b7ad9090ff29405
ohahaha29 <ohahaha29@gmail.com>
2021-03-01 16:19:29 +0900

    여러 개의 파라미터 실습

--- in_javascript/index.js
+++ in_javascript/index.js
@@ -1,18 +1,9 @@
 //여러 개의 파라미터
-function printSum(num1, num2) {
-    console.log(num1 + num2);
-};
+function bmiCalculator(name, weight, tall) {
+    console.log(name + "님의 체질량지수는 " + weight/(tall*tall/10000) + "입니다.");
+}
 
-printSum(10, 5);
-printSum(123, 4);
-
-function introduce(name, birth, nationality, job) {
-    console.log("안녕하세요! 반갑습니다.");
-    console.log("제 이름은 " + name + "입니다.");
-    console.log("생년월일은 " + birth + "이고,");
-    console.log("국적은 " + nationality + "입니다.");
-    console.log("직업은 " + job + "입니다.");
-    console.log("잘 부탁드립니다.");
-};
-
-introduce("코드잇", "17.05.18", "한국", "프로그래머");
+// 테스트
+bmiCalculator('홀쭉이', 43.52, 160);
+bmiCalculator('코린이', 61.25, 175);
+bmiCalculator('통통이', 77.76, 180);
