744766b23827bff5190715ba89d061a82bb812dc
ohahaha29 <ohahaha29@gmail.com>
2021-03-01 21:08:55 +0900

    자료형 응용하기

--- in_javascript/index.js
+++ in_javascript/index.js
@@ -1,10 +1,23 @@
-function calcWage(name, time, wage) {
-    let total = time * wage;
+// 숫자형과 문자열 재료
+let material1 = 3;
+let material2 = '3';
+let material3 = 10;
+let material4 = '4';
+let material5 = 4;
 
-    console.log(`${name}님의 근무 시간은 총 ${time}시간이며, 최종 급여는 ${time * wage}원 입니다.`); // 코드를 채워 넣으세요.
-}
+// 연산 결과
+let result1;
+let result2;
 
-calcWage('김윤식', 208, 11340);
-calcWage('성규재', 175, 12160);
-calcWage('손태웅', 161, 13070);
-calcWage('허우선', 222, 10980);
+// 연산을 통해 result1에 문자열 '34'를, result2에 숫자형 34를 만들어 넣어주세요.
+// 코드를 작성해주세요.
+result1 = material2 + material4;
+result1 = material1 + String(material5);
+result2 = material1 * material3 + material5;
+result2 = Number(material2 + material4)
+
+// 테스트 코드
+console.log(result1);
+console.log(typeof result1);
+console.log(result2);
+console.log(typeof result2);
