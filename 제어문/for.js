52164fb4224443bc04528d33812d3377fc3ba85b
ohahaha29 <ohahaha29@gmail.com>
2021-03-02 21:13:50 +0900

    for문

--- 제어문/for.js
+++ 제어문/for.js
@@ -24,3 +24,34 @@ for (let i = 1; i <= 10; i++) { // 반복 횟수를 저장할 변수 생성; 변
     console.log('코드잇 최고'); // 초기화 부분 -> 조건 부분 -> 동작 부분 -> 추가 동작 부분 -> 조건 부분 -> 동작 부분 -> 추가 동작 부분 -> i가 11이 될 때까지 반복
 }
 
+// for문 연습(1)
+for (let i = 2; i <= 100; i += 2) {
+    console.log(i);
+}
+
+// for문 연습(2)
+/* 파라미터로 전달되는 숫자 값에 따라 *을 출력하는 코드
+function printTriangle(height) {
+    for (let i = 0; i < height; i++) {
+        console.log('*');
+    }
+} */
+/* 반복할 때마다 *의 개수를 증가시켜주기 위해서, 콘솔에 *을 바로 출력하는 게 아니라
+for문이 반복되기 전에 message라는 변수를 만든 다음 for문이 반복될 때마다 message에 *을 하나씩 추가하면서 콘솔에 message를 출력 */
+function printTriangle(height) {
+    let message = '';
+    for (let i = 0; i < height; i++) {
+        message += '*';
+        console.log(message);
+    }
+}
+
+// 테스트 코드
+console.log('높이: 1');
+printTriangle(1);
+
+console.log('높이: 3');
+printTriangle(3);
+
+console.log('높이: 5');
+printTriangle(5);
