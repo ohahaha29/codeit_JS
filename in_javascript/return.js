9b667d638956c6fbe048233c82a560d90c9dd71e
ohahaha29 <ohahaha29@gmail.com>
2021-03-02 13:13:14 +0900

    return문 실습

--- in_javascript/return.js
+++ in_javascript/return.js
@@ -20,3 +20,19 @@ console.log('함수 호출 후');
 return 전
 9
 함수 호출 후 */
+
+
+// return 실습
+function calculateRectangleArea(width, height) {
+    return width * height  
+}
+
+// 테스트 코드
+    let area1 = calculateRectangleArea(3, 4); // 가로 3, 세로 4인 직사각형의 넓이 계산
+    let area2 = calculateRectangleArea(5, 8); // 가로 5, 세로 8인 직사각형의 넓이 계산
+    let area3 = calculateRectangleArea(7, 2); // 가로 7, 세로 2인 직사각형의 넓이 계산
+
+console.log(`Area1: ${area1}, Area2: ${area2}, Area3: ${area3}`);
+
+// 결과
+// Area1: 12, Area2: 40, Area3: 14
