2756b72d3c66bdb41bf331229b35dad221ecc9f3
ohahaha29 <ohahaha29@gmail.com>
2021-03-03 13:51:31 +0900

    object_property 실습

--- 프로그래밍&데이터/object_property.js
+++ 프로그래밍&데이터/object_property.js
@@ -40,3 +40,18 @@ console.log(codeit[propertyName]);
 // 3. 객체 안에 객체로 접근하는 방법
 console.log(codeit.bestCourse.title);
 console.log(codeit.bestCourse['title']);
+
+
+// 객체와 프로퍼티 실습 - 영어 단어장
+let myVoca = {
+	function: '함수',
+	variable: '변수',
+	constant: '상수',
+	local: '지역의',
+	global: '전반적인'
+}
+
+console.log(myVoca);
+console.log(myVoca.local);
+console.log(myVoca.constant);
+console.log(myVoca.function);
