6338845d1e63434fb9e18871301435f51da582cf
ohahaha29 <ohahaha29@gmail.com>
2021-03-04 14:57:49 +0900

    multidimensional_array 실습

--- 배열/multidimensional_array.js
+++ 배열/multidimensional_array.js
@@ -4,3 +4,27 @@ let twoDimensional = [[1, 2], [3, 4]];
 
 console.log(twoDimensional[0]) // 출력: [1, 2]
 console.log(twoDimensional[0][1]) // 출력: [2]
+
+
+// 다차원 배열 실습 - 팀 나누기
+let groups = [
+    ['영준', '캡틴'], 
+    ['태순', '우재'],
+    ['재훈', '지웅'],
+    ['윤형', '동욱'],
+    ['규식', '소원']
+]
+
+let teams = [
+    [],
+    [],
+]
+
+for(let i = 0; i < groups.length; i++) {
+    for(let j = 0; j < groups[i].length; j++) { // 2차원 배열을 다루기 위해 반복문을 겹치면 각 요소별로 값들을 가져올 수 있다.
+    teams[j][i] = groups[i][j]; // groups의 각 index를 뒤집어서 teams에 할당해주면 간단하게 배열 나누기 가능
+    }
+}
+
+console.log(teams[0]);
+console.log(teams[1]);
