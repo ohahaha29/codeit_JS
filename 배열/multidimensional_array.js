d26db04e8bc746eb33e90ffc48d82ffdf8474661
ohahaha29 <ohahaha29@gmail.com>
2021-03-04 14:42:07 +0900

    multidimensional_array

---
+++ 배열/multidimensional_array.js
@@ -0,0 +1,6 @@
+// 다차원 배열(multidimensional array): 배열의 요소에 배열이 들어가는 형태
+// 여러 가지 값들을 가지고서 순서나 위치에 중점을 두는 정보가 필요할 때 활용
+let twoDimensional = [[1, 2], [3, 4]];
+
+console.log(twoDimensional[0]) // 출력: [1, 2]
+console.log(twoDimensional[0][1]) // 출력: [2]
